3d534293b2d5a3a8ba4f5d358a0667edf8b6516a
Lalitha Rani U <124855730+lalitharani03@users.noreply.github.com>
2024-04-16 16:08:51 +0530

    Add files via upload

---
+++ Day5_practiceathome/src/main/java/com/day5/practiceathome/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.day5.practiceathome;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PracticeathomeApplication.class);
+	}
+
+}
